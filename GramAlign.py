bc4e0cfc0bd9169f3bfc07f816f527dabd9b5a16
maximato <maxxxnes@gmail.com>
2019-11-19 10:10:06 +0300

    Remove RunnerGramAlign, save functionality
    RunnerGramAlign class was removed, function was save in GramAlign class

--- GramAlign.py
+++ GramAlign.py
@@ -8,6 +8,14 @@ class GramAlign:
         print(command)
         #os.system(command)
 
+    @staticmethod
+    def run_for_all_in(directory):
+        ga = GramAlign()
+        files = os.listdir(directory)
+        for file in files:
+            ga.run_gram_align(file)
+
 
 ga = GramAlign()
 ga.run_gram_align("file.fasta")
+ga.run_for_all_in("Groups")
